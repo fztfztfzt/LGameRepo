f320df49d2371fb094e71fda341bd82d2f3ddcde
Allen Kashiwa <358804906@qq.com>
2015-12-20 13:34:51 +0800

    大家游戏开始状态框架

--- LGameProject/Assets/Scripts/GameState/GameStartState.cs
+++ LGameProject/Assets/Scripts/GameState/GameStartState.cs
@@ -15,7 +15,49 @@ public class GameStartState : FSMState
     {
         Init();
     }
+    /************************* LOGO界面 START *************************/
 
+    //是否显示游戏启动LOGO
+    private bool mIsShowGameLogo = true;
+    public bool IsShowGameLogo
+    {
+        get { return mIsShowGameLogo; }
+        set { mIsShowGameLogo = value; }
+    }
+
+    private void ShowGameLogo()
+    {
+        if (mIsShowGameLogo == true)
+        {
+            //TODO 显示LOGO
+
+            mIsShowGameLogo = false;
+            //LOGO显示完毕，显示主菜单
+            mIsShowMainMenu = true;
+        }
+    }
+    /************************* LOGO界面 END *************************/
+    /************************* 主菜单界面 START *************************/
+
+    private bool mIsShowMainMenu = false;
+    public bool IsShowingMainMenu
+    {
+        get { return mIsShowMainMenu; }
+        set { mIsShowMainMenu = value; }
+    }
+
+    public void ShowMainMenu()
+    {
+        if (mIsShowMainMenu == true)
+        {
+            //TODO 显示主菜单
+
+            mIsShowMainMenu = false;
+        }
+    }
+    /************************* 主菜单界面 START *************************/
+
+    /************************* 状态机部分接口 START *************************/
     public override void Init()
     {
         Utils.DBG(string.Format("{0} Init!", StateName));
@@ -29,6 +71,10 @@ public class GameStartState : FSMState
     public override void Execute()
     {
         Utils.DBG(string.Format("{0} Execute!", StateName));
+        //显示游戏LOGO界面
+        ShowGameLogo();
+        //显示游戏主菜单
+        ShowMainMenu();
     }
 
     public override void OnMsg(string msg)
@@ -40,4 +86,5 @@ public class GameStartState : FSMState
     {
         throw new System.NotImplementedException();
     }
+    /************************* 状态机部分接口 END *************************/
 }
